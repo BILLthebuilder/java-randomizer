5e6b6f52536ab2693407c1d0f3f04029ba4dd96b
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-06-20 19:50:42 +0300

    program to print two a random number between two inputs

--- Inputs.java
+++ Inputs.java
@@ -1,3 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Inputs {
-    
+    static Random rand = new Random();
+    static Scanner scanner = new Scanner(System.in);
+
+    static int numbers() {
+        System.out.println("Enter the first number");
+        int min = scanner.nextInt();
+        System.out.println("Enter the second number");
+        int max = scanner.nextInt();
+        int result = rand.nextInt((max - min) + 1) + min;
+        System.out.println("The random number is:");
+        try {
+            System.out.println(result); 
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return result;  
+    }
+    public static void main(String args[]) {
+        numbers();
+    }
 }
