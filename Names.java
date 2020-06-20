2161552d3288cd47bd42ff5d8d2fe468a8457ab1
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-06-20 19:51:15 +0300

    this one prints random names from an array

--- Names.java
+++ Names.java
@@ -1,3 +1,11 @@
+import java.util.Random;
 public class Names {
-    
+    static String names[] = { "Eugenia", "Brian", "Michelle",
+    "Lorine", "John", "Jedidah", "Anthony", "Maulid",
+            "Beatrice", "Bill", "Rufus" };
+    static Random random = new Random();
+    public static void main(String args[]) {
+        int select = random.nextInt(names.length);
+        System.out.println("The random name is:"+names[select]);
+    }
 }
