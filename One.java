ab301e23d3fb76182d37155663eb06b22996ed8e
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-06-20 18:29:16 +0300

    simple script for random numbers between 1 and zero

--- One.java
+++ One.java
@@ -1,17 +1,8 @@
-import java.util.*;
+import java.util.Random;
 
 public class One {
-   
-    Random rand = new Random();
-    int numbers() {
-    int first = 0;
-    int second = 1;
-    int result = rand.nextInt(first - second) + second;
-    return result;
-    }
-
+    static Random rand = new Random();
     public static void main(String args[]) {
-        One o = new One();
-     System.out.println(o.numbers());
+     System.out.println(rand.nextDouble());
     }
 }
