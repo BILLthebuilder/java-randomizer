a5c1943cff92090f4dc310fac6288651c193c9f3
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-06-23 09:01:51 +0300

    arrays initial intro

---
+++ Arrays.java
@@ -0,0 +1,14 @@
+public class Arrays {
+    public static void main(String args[]) {
+        int[] number;
+        number = new int[4];
+        number[0] = 6;
+        number[1] = 7;
+        number[2] = 8;
+        number[3] = 90;
+
+        for (int i = 0; i < number.length; i++) {
+            System.out.println(number[i]);
+        }
+    }
+}
