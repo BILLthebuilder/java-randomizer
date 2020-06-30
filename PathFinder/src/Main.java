8f5ac8ba25ab6e477af801ce491db5ea6f94fcdf
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-06-30 08:07:51 +0300

    create pathfinder program

---
+++ PathFinder/src/Main.java
@@ -0,0 +1,29 @@
+import java.text.CharacterIterator;
+import java.util.HashMap;
+
+public class Main {
+
+    HashMap<String, Integer> cityA;
+    HashMap<String, Integer> cityB;
+    HashMap<String, Integer> cityC;
+    HashMap<String, Integer> cityD;
+    HashMap<String, Integer> cityE;
+    Main(){
+
+    }
+    /**
+     * cityA = [
+     * cityB = 45,
+     * cityC = 80
+     * ]
+     *
+     * cityB = [
+     * cityE = 78,
+     * cityD = 98
+     * ]
+     * @param args
+     */
+    public static void main(String[] args){
+
+    }
+}
